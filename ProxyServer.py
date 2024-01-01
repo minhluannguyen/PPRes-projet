dad1960dc37edd8a411f58a3a60e634902c07c86
Lundi <minhluannguen@gmail.com>
2024-01-01 06:36:18 +0100

    save config filter to txt

--- ProxyServer.py
+++ ProxyServer.py
@@ -3,14 +3,16 @@ import threading
 import signal
 import sys
 import re
-import ssl
 
 class ProxyServer:
-    def __init__(self, port):
+    def __init__(self, port, censorRulesFile, replaceRulesFile):
         signal.signal(signal.SIGINT, self.close)
         self.port = port
+        self.censorRulesFile = censorRulesFile
+        self.replaceRulesFile = replaceRulesFile
 
-        self.clients = []
+        self.censorRules = self.readListFileRules(self.censorRulesFile)
+        self.replaceRules = self.readListFileRules(self.replaceRulesFile)
 
         #Socket init
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -19,13 +21,17 @@ class ProxyServer:
         self.serverSocket.listen(socket.SOMAXCONN)
         print("Listening on port " + str(port) + "...")
 
+    def readListFileRules(self, file):
+        with open(file, "rb") as f:
+            return list(map(lambda x:x.lower(), f.read().strip().splitlines()))
+
     def recvall(self, socket, timeout = 1):
         # recv all data
         data = b""
         socket.settimeout(timeout) # because browser not end TCP so we need to set timeout for _socket.recv
         try:
             while 1:
-                buffer = socket.recv(1024) # receive buffer
+                buffer = socket.recv(4096) # receive buffer
                 if (not buffer):
                     break
                 data += buffer # add buffer to data
@@ -34,9 +40,10 @@ class ProxyServer:
         return data
     
     def forward_data(self, source, destination):
-        print("443------------------2")
+        source.settimeout(1)
+        destination.settimeout(1)
         while True:
-            data = source.recv(4096)
+            data = source.recv(1024)
             if not len(data):
                 break
             destination.send(data)
@@ -44,6 +51,7 @@ class ProxyServer:
         source.close()
         destination.close()
     
+    #Filter header before send to server
     def filterHeader(self, data):
         data = data.replace(b'HTTP/1.1', b'HTTP/1.0')
 
@@ -53,7 +61,6 @@ class ProxyServer:
             "Accept-Encoding: gzip"
         ]
         headerList = data.decode().split('\r\n')
-        # print(headerList)
         filteredList = []
         for field in headerList:
             flag = True
@@ -64,11 +71,10 @@ class ProxyServer:
             if flag:
                 filteredList.append(field)
         
-        #filteredList.append("\r\n")
         header = "\r\n".join(filteredList).encode()
-        
         return header
     
+    #Filter data before send to client
     def filterData(self, data):
         try:
             dataSegment = data.split(b"\r\n\r\n")
@@ -78,40 +84,33 @@ class ProxyServer:
             body = b''.join(dataSegment[1:])
 
             # Modify title
-            body = re.sub(b'<title>.*?</title>', b'<title>Sufing with Proxy Server</title>', body)
+            body = re.sub(b'<title>.*?</title>', b'<title>Surfing with Proxy Server</title>', body)
 
             # Delete all mp4 resources
             body = re.sub(rb'<source.*?type="video/mp4".*?>', b'', body)
 
-            # Censor forhibited words
-            prohibitedList = [
-                'HTML',
-                'site',
-                'récompense'
-            ]
-            for word in prohibitedList:
-                body = re.sub(word.encode(), b'*' * len(word), body, flags=re.IGNORECASE)
+            # Censor forhibited words, ignore the html tags too ...
+            for word in self.censorRules:
+                body = re.sub(rb"(?<!<[^>])(%s)(?![^<]*>)" % word, b'*' * len(word), body, flags=re.IGNORECASE)
 
-            # Replace word in giving list
-            replaceList = [
-                'This:JavaFuckingScript',
-                '2023:2024',
-            ]
-            
-            for rule in replaceList:
-                key = rule.split(":")[0].encode()
-                val = rule.split(":")[1].encode()
-                body = re.sub(key, val, body, flags=re.IGNORECASE)
+            # Replace word in giving list            
+            for rule in self.replaceRules:
+                key = rule.split(b':')[0]
+                val = rule.split(b':')[1]
+                body = re.sub(rb"(?<!<[^>])(%s)(?![^<]*>)" % key, val, body, flags=re.IGNORECASE)
             
             # Recreate response
             data = b"\r\n\r\n".join([header, body])
-        except Exception:
+        except Exception as e:
+            print(e)
             pass
         return data
 
+    #get client's URL from request
     def parseURL(self, request):
         return request.decode().split(' ')[1]
     
+    #Get client's hostname from request
     def parseHost(self, request):
         if b"Host" in request:
             hostPos = request.find(b"Host") + 6
@@ -132,7 +131,6 @@ class ProxyServer:
         while 1:
             (clientSocket, TSAP) = self.serverSocket.accept()
             print("New connection from: " + str(TSAP))
-            self.clients.append(clientSocket)
             thread = threading.Thread(name= TSAP, target = self.client_proxy, args=(clientSocket, TSAP), daemon=True)
             thread.start()
 
@@ -142,43 +140,57 @@ class ProxyServer:
         if len(request) == 0:
             return clientSocket.close()
 
+        # Get client's hostname, port, and URL
         serverAddress, serverPort = self.parseHost(request)
         serverURL = self.parseURL(request)
         print(serverAddress, serverPort, serverURL)
 
-        #Filtering
-        #header = request.split(b"\r\n\r\n")[0]
-        request = self.filterHeader(request)
-        #request = header
-        #Censoring
-
-        #Serving
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serverSocket.connect((serverAddress, serverPort))
-        if serverPort == 443:
-            # context = ssl.create_default_context()
-            # serverSocket = context.wrap_socket(sock=serverSocket, server_hostname=serverAddress)
-            # serverSocket.connect((serverAddress, serverPort))
 
-            clientSocket.sendall(b"HTTP/1.1 200 Connection established\r\n\r\n")
-            self.forward_data(serverSocket, clientSocket)
+        #Forward HTTPS connections
+        if serverPort == 443:
+            try:
+                # context = ssl.create_default_context()
+                # serverSocket = context.wrap_socket(sock=serverSocket, server_hostname=serverAddress)
+                # serverSocket.connect((serverAddress, serverPort))
+
+                clientSocket.sendall(b"HTTP/1.1 200 Connection established\r\n\r\n")
+                self.forward_data(serverSocket, clientSocket)
+            except Exception as e:
+                print(e)
+            finally:
+                clientSocket.close()
+                serverSocket.close()
+                return
         
-        print(request)
-        print("------------------")
+        #Change header before send to server
+        request = self.filterHeader(request)
         serverSocket.sendall(request)
 
-        data = self.recvall(serverSocket)
-        if not data:
-            return
-        data = self.filterData(data)
-        clientSocket.sendall(data)
-        # print(data.decode())
+        # print(request)
+        # print("------------------")
+
+        #receive all response from server and send back to client
+        while True:
+            data = self.recvall(serverSocket)
+            print(data)
+            if not data:
+                break
+            data = self.filterData(data) #filter data before send to client
+            clientSocket.sendall(data)
+        
+        serverSocket.close()
+        return clientSocket.close()
 
     def close(self):
         self._socket.close()
         sys.exit(0)
 
-server = ProxyServer(1234)
+server = ProxyServer(port=8080,
+                     censorRulesFile="censorRules.txt",
+                     replaceRulesFile="replaceRules.txt"
+                    )
 server.start()
 
 
